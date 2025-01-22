01cfa416d9cdb4e607e69cea24d453784bfa9f98
Konrad G <konradg.net@gmail.com>
2025-01-22 11:30:41 +0100

    Removed remoteip from Turnstile

--- server/cloudflare/turnslide/CloudflareTurnstileServer.tsx
+++ server/cloudflare/turnslide/CloudflareTurnstileServer.tsx
@@ -4,19 +4,17 @@ export class CloudflareTurnstileServer {
 
     public static async isRequestValidated(req: Request): Promise<boolean> {
         const token = req.body["cf-turnstile-response"]
-        const ip: any = req.headers["CF-Connecting-IP"]
-  
+
         const formData = new FormData()
         formData.append("secret", process.env.CLOUDFLARE_TURNSLIDE_KEY_SERVER)
         formData.append("response", token)
-        formData.append("remoteip", ip)
   
         const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify"
         const result = await fetch(url, {
           body: formData,
           method: "POST"
         })
-  
+        
         const outcome = await result.json()
         return outcome.success
     }
