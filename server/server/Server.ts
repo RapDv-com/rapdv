335069a5100e23944ffa3f5503f323b8eb822578
Konrad G <konradg.net@gmail.com>
2025-01-22 13:47:09 +0100

    Revert "Experimented with Bun.build"
    This reverts commit e7a5c66f5b1da8a85f62ff88f42aaa34faa49362.

--- server/server/Server.ts
+++ server/server/Server.ts
@@ -17,7 +17,6 @@ import { PageSetup } from "../pages/PageSetup"
 import { Auth } from "../auth/Auth"
 import express from "express"
 import chokidar from 'chokidar';
-import Bun from "bun";
 
 export class Server {
 
@@ -77,19 +76,6 @@ export class Server {
       })
 
       await this.setupApp()
-
-       console.log("Building client files...")
-      const buildResults = await Bun.build({
-        entrypoints: ['./client/App.tsx'],
-        outdir: './dist',
-        splitting: false,
-        minify: this.isProduction,
-      });
-      if(buildResults.success) {
-        console.log("✅ Client files built successfully")
-      } else {
-        console.error("❌ Couldn't build client files. ", buildResults.logs)
-      }
 
       if (!this.isProduction) {
 
