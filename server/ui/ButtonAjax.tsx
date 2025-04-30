3cdfd965cae4d2e111f1bef8ca512c4626afebc9
Konrad G <konradg.net@gmail.com>
2025-04-30 12:50:19 +0200

    Added noPjax option to Ajax

--- server/ui/ButtonAjax.tsx
+++ server/ui/ButtonAjax.tsx
@@ -7,16 +7,19 @@ type Props = {
   children?: any
   params?: Object
   method?: ReqType
+  noPjax?: boolean
 }
 
 export class ButtonAjax extends React.Component<Props & React.FormHTMLAttributes<HTMLFormElement>> {
   render(): ReactNode | string {
-    const { children, className, method, params, ...otherProps } = this.props
+    const { children, className, method, params, noPjax, ...otherProps } = this.props
     if (otherProps.hidden) {
       return null
     }
+    const pjaxClass = noPjax ? "" : "pjax"
+    
     return (
-      <form {...otherProps} className="pjax" method={method ?? ReqType.Post} encType="multipart/form-data" noValidate>
+      <form {...otherProps} className={pjaxClass} method={method ?? ReqType.Post} encType="multipart/form-data" noValidate>
         <input type="hidden" name="_csrf" value="{{_csrf}}" />
         {!!params && Object.keys(params).map((key, index) => <input key={index} type="hidden" name={key} value={params[key]} />)}
         <button type="submit" className={className}>
