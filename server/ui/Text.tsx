bfab7877d5f19bb76f2050da2ae4993aab653902
Konrad G <konradg.net@gmail.com>
2025-09-23 01:37:13 +0200

    Clean up

--- server/ui/Text.tsx
+++
@@ -1,9 +0,0 @@
-// Copyright (C) Konrad Gadzinowski
-
-import React, { ReactNode } from "react"
-
-export class Text extends React.Component<{ text: string }> {
-  render(): ReactNode | string {
-    return <div>{this.props.text}</div>
-  }
-}
