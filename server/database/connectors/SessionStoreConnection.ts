505eb9af0d7e043a7ecf7061ca84bd3ef73cfa1e
Konrad G <konradg.net@gmail.com>
2026-04-27 22:44:03 +0200

    Updated comments

--- server/database/connectors/SessionStoreConnection.ts
+++ server/database/connectors/SessionStoreConnection.ts
@@ -1,5 +1,5 @@
 export class SessionStoreConnection {
-  
+
   public static async close(sessionStore: any): Promise<void> {
     const closeMethod = sessionStore?.close
     if (typeof closeMethod !== 'function') {
@@ -7,6 +7,7 @@ export class SessionStoreConnection {
     }
 
     if (closeMethod.length > 0) {
+      // Handle callback
       await new Promise<void>((resolve, reject) => {
         closeMethod((error: any) => {
           if (error) {
