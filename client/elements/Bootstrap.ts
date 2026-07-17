0d2d1594ee3e1ab6d37522d35986fbd96ed2ac9b
Konrad G <konradg.net@gmail.com>
2026-07-17 09:10:53 +0200

    Defined Bootstrap elements

---
+++ client/elements/Bootstrap.ts
@@ -0,0 +1,15 @@
+// Copyright (C) Konrad Gadzinowski
+
+import bootstrap from "bootstrap/dist/js/bootstrap.js"
+
+export const Modal = bootstrap.Modal
+export const Dropdown = bootstrap.Dropdown
+export const Tooltip = bootstrap.Tooltip
+export const Toast = bootstrap.Toast
+export const Carousel = bootstrap.Carousel
+
+export type Modal = any
+export type Dropdown = any
+export type Tooltip = any
+export type Toast = any
+export type Carousel = any
